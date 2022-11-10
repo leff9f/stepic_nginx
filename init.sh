a5ed8713f8c829c51295be66a0c631359539fc4d
i.churilov <i.churilov@2gis.ru>
2022-11-10 22:08:43 +0200

    config

--- init.sh
+++ init.sh
@@ -1,4 +1,2 @@
-sudo mv /home/box/StepikNginx/web ~
-sudo ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
-sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx.conf
 sudo /etc/init.d/nginx restart
