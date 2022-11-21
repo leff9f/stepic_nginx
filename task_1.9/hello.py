ecde2c71b270e99be9a1c84cd46c711e4e5ed330
i.churilov <i.churilov@2gis.ru>
2022-11-21 19:57:31 +0200

    start response

--- task_1.9/hello.py
+++ task_1.9/hello.py
@@ -1,3 +1,9 @@
 def app(environ, start_response):
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ('Content-Length', str(len(environ)))
+    ]
+    start_response(status, response_headers)
     body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     return body
